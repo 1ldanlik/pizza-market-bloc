b7bb829f2b6c7ff41152431aa53f306e4ecbdba8
Ildan <ildanlik2002@gmail.com>
2022-10-25 22:05:07 +0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/pizza_market_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pizza_market_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pizza Market'),
+        actions: [
+          IconButton(onPressed: () {
+
+          }, icon: const ImageIcon(AssetImage('assets/basket_icon.png'))),
+          IconButton(onPressed: () {
+
+          }, icon: const ImageIcon(AssetImage('assets/person_icon.png')))
+        ],
+      ),
+    );
+  }
+}
